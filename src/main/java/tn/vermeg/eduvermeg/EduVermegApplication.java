ab0cb02dadc90aa54aab2f21d089b841195ac018
maissahasni <101124164+maissahasni@users.noreply.github.com>
2025-07-28 22:24:09 +0100

    first commit

---
+++ src/main/java/tn/vermeg/eduvermeg/EduVermegApplication.java
@@ -0,0 +1,13 @@
+package tn.vermeg.eduvermeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduVermegApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduVermegApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/vermeg/eduvermeg/EduVermegApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.vermeg.eduvermeg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduVermegApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
